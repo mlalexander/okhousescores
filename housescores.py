e3b9edeb898a0f5c2a881a9018bd8d4c275f1736
Madi Alexander <madi.alexander@gmail.com>
2015-05-10 23:29:00 -0500

    updated scripts, they actually work and take forever to run

---
+++ housescores.py
@@ -0,0 +1,48 @@
+import csv
+from sunlight import openstates
+
+ok_legislators = openstates.legislators(
+    state='ok',
+    active='true'
+)
+
+ok_legislators_csv_key = ['leg_id']
+ok_legislators_array = []
+for legislator in ok_legislators:
+    ok_legislators_csv_key.append(legislator['leg_id'])
+    ok_legislators_array.append(legislator['leg_id'])
+
+with open('housescores.csv', 'w') as w:
+    writer = csv.DictWriter(w, fieldnames=ok_legislators_csv_key, extrasaction='ignore')
+    writer.writeheader()
+
+    for legislatorA in ok_legislators_array:
+        
+        leg_scores = {}
+        leg_scores['leg_id'] = legislatorA
+
+        for legislatorB in ok_legislators_array:
+
+            with open('housevotes.csv') as f:
+                reader = csv.DictReader(f)
+
+                voteTotal = 0
+                match = 0
+                noMatch = 0
+
+                for bill in reader:
+
+                    if not bill[legislatorA] or not bill[legislatorB]:
+
+                        noMatch += 1
+
+                    elif bill[legislatorA] == bill[legislatorB]:
+
+                        voteTotal += 1
+                        match += 1
+
+                    else:
+
+                        voteTotal += 1
+
+        writer.writerow(leg_scores)
---
+++ housevotes.py
@@ -0,0 +1,57 @@
+from sunlight import openstates
+import csv
+import re
+
+oklahoma_lower_bills = openstates.bills(
+    state='ok',
+    search_window='term:2015-2016'
+)
+
+#retrieves legislators in the house,
+ok_legislators = openstates.legislators(
+    state='ok',
+    chamber='lower',
+    active='true'
+)
+
+ok_legislators_array = ['bill_id', 'chamber', 'vote_id']
+for legislator in ok_legislators:
+    ok_legislators_array.append(legislator['leg_id'])
+
+with open('housevotes.csv', 'w') as f:
+    writer = csv.DictWriter(f, fieldnames=ok_legislators_array, extrasaction='ignore')
+
+    writer.writeheader()
+
+#need to add something to filter out bill actions that occur in the opposite chamber, but can't figure that out
+    for bill in oklahoma_lower_bills:
+        oklahoma_bill_details = openstates.bill_detail(
+            state='ok',
+            session='2015-2016',
+            bill_id=bill['bill_id']
+        )
+
+        for bill_votes in oklahoma_bill_details['votes']:
+
+            pattern = re.compile('third reading', re.IGNORECASE)
+
+            if pattern.search(bill_votes['motion']):
+
+                total_votes = {}
+
+                total_votes['bill_id'] = bill_votes['bill_id']
+                total_votes['vote_id'] = bill_votes['vote_id']
+                total_votes['chamber'] = bill_votes['chamber']
+
+
+                for yes_votes in bill_votes['yes_votes']:
+                    total_votes[yes_votes['leg_id']] = 1
+
+                for no_votes in bill_votes['no_votes']:
+                    total_votes[no_votes['leg_id']] = 2
+
+                try:
+                    writer.writerow(total_votes)
+
+                except ValueError:
+                    print "error"
